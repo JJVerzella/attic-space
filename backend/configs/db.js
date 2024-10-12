936e839a827d35680ca999913da29a91a86d3753
JJ Verzella <jj.verzella@gmail.com>
2024-10-12 10:53:05 -0400

    Update connection

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,12 +3,8 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
     try {
-        let value = JSON.parse(process.env.SECRETS);
-        console.log(value)
-        console.log(value.MONGO_URI);
-        console.log(process.env.SECRETS);
         console.log(process.env.SECRETS.MONGO_URI);
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.SECRETS.MONGO_URI);
     }
     catch (e) {
         console.error('Connection failed');
